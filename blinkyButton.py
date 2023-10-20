2583d0a3301f0a6bdc193850a5cd0d9c4dac9c62
anudsa <anwardelgadosilva@gmail.com>
2023-10-20 17:09:16 -0600

    Original Commit

---
+++ blinkyButton.py
@@ -0,0 +1,11 @@
+#Code to make an LED blink with a push button
+from gpiozero import LED,Button
+from signal import pause
+#A new LED object is created and the gpio pin 26 is used
+blueLED=LED(26)
+#A new button object is instantiated with the gpio pin 2
+pushButton=Button(2)
+#The method blink is called
+blueLED.blink()
+#A pause is added so that it can be seen how LED blinks
+pause()
